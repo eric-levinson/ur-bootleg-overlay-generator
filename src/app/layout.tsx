d38f55f5b1c67c8f21abd809f98ed2dab06faa69
Eric <eric.levinson42@gmail.com>
2023-08-06 20:10:28 -0500

    head and directus api fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@
 
 import './globals.css'
 import {Providers} from "./providers";
+import Head from './head';
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en" className='h-full'>
+      <Head />
       <body>
         <Providers>
           {children}
--- src/pages/api/directus.js
+++ src/pages/api/directus.js
@@ -1,5 +1,5 @@
 const directusurl = process.env.DIRECTUS_API_URL
 import { createDirectus } from '@directus/sdk';
 import { rest } from '@directus/sdk/rest';
-const directus = createDirectus("https://cntrpnt.io/").with(rest());
+const directus = createDirectus(directusurl).with(rest());
 export default directus;
